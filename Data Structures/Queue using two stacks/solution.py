4b7f683f345191774a3644cd567ec337f687f792
Filip Jeremic <fjeremicwork@gmail.com>
2020-10-18 17:21:50 +0200

    Added the method complexity information to the "Queue using two stacks" Python3 solution

--- Data Structures/Queue using two stacks/solution.py
+++ Data Structures/Queue using two stacks/solution.py
@@ -5,14 +5,17 @@
 class Queue:
 
    def __init__(self):
+      # O(1)
       # basically, one stack for input and one for output
       self.enqueueStack = []
       self.dequeueStack = []
 
    def enqueue(self, value):
+      # amortized O(1)
       self.enqueueStack.append(value)
    
    def dequeue(self):
+      # amortized O(1)
       # only transfer when the output stack is empty
       self.__check_and_transfer()
       # checking again because the source stack can also be empty (we transferred nothing)
@@ -21,15 +24,18 @@ class Queue:
       return self.dequeueStack.pop()
    
    def __check_and_transfer(self):
+      # O(n), where n is the number of elements in enqueueStack
       if self.dequeueStack:
          return
       self.__transfer()
    
    def __transfer(self):
+      # O(n), where n is the number of elements in enqueueStack
       while self.enqueueStack:
          self.dequeueStack.append(self.enqueueStack.pop())
    
    def front(self):
+      # amortized O(1)
       self.__check_and_transfer()
       if not self.dequeueStack:
          return None
