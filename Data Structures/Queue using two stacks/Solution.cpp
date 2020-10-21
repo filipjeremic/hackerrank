6cea96118912857ec34a5adff6dfd6dba34b7bba
Filip Jeremic <fjeremicwork@gmail.com>
2020-10-21 12:08:56 +0200

    Updated formatting

--- Data Structures/Queue using two stacks/Solution.cpp
+++ Data Structures/Queue using two stacks/Solution.cpp
@@ -5,92 +5,92 @@
 
 class Queue
 {
-   // one stack for input and one for output
-   std::stack<int> m_enqueueStack;
-   std::stack<int> m_dequeueStack;
+    // one stack for input and one for output
+    std::stack<int> m_enqueueStack;
+    std::stack<int> m_dequeueStack;
 
-   void transfer()
-   {
-      while (!m_enqueueStack.empty())
-      {
-         const int value = m_enqueueStack.top();
-         m_enqueueStack.pop();
-         m_dequeueStack.push(value);
-      }
-   }
+    void transfer()
+    {
+        while (!m_enqueueStack.empty())
+        {
+            const int value = m_enqueueStack.top();
+            m_enqueueStack.pop();
+            m_dequeueStack.push(value);
+        }
+    }
 
-   void checkAndTransfer()
-   {
-      // only transfer when the output stack is empty
-      if (!m_dequeueStack.empty())
-      {
-         return;
-      }
+    void checkAndTransfer()
+    {
+        // only transfer when the output stack is empty
+        if (!m_dequeueStack.empty())
+        {
+            return;
+        }
 
-      transfer();
-   }
+        transfer();
+    }
 
 public:
-   void enqueue(const int value)
-   {
-      m_enqueueStack.push(value);
-   }
+    void enqueue(const int value)
+    {
+        m_enqueueStack.push(value);
+    }
 
-   int dequeue()
-   {
-      checkAndTransfer();
+    int dequeue()
+    {
+        checkAndTransfer();
 
-      // checking again because the enqueue stack can also be empty (we transferred nothing)
-      if (m_dequeueStack.empty())
-      {
-         throw 1;
-      }
+        // checking again because the enqueue stack can also be empty (we transferred nothing)
+        if (m_dequeueStack.empty())
+        {
+            throw 1;
+        }
 
-      const int value = m_dequeueStack.top();
-      m_dequeueStack.pop();
-      return value;
-   }
+        const int value = m_dequeueStack.top();
+        m_dequeueStack.pop();
+        return value;
+    }
 
-   int front()
-   {
-      checkAndTransfer();
+    int front()
+    {
+        checkAndTransfer();
 
-      if (m_dequeueStack.empty())
-      {
-         throw 1;
-      }
+        if (m_dequeueStack.empty())
+        {
+            throw 1;
+        }
 
-      return m_dequeueStack.top();
-   }
+        return m_dequeueStack.top();
+    }
 };
 
 int main()
 {
-   int n = 0;
-   std::cin >> n;
+    int n = 0;
+    std::cin >> n;
 
-   Queue q;
+    Queue q;
 
-   for (int i = 0; i < n; i++)
-   {
-      int type = 0;
-      std::cin >> type;
+    for (int i = 0; i < n; i++)
+    {
+        int type = 0;
+        std::cin >> type;
 
-      if (type == 2)
-      {
-         q.dequeue();
-      }
-      else if (type == 3)
-      {
-         std::cout << q.front() << '\n';
-      }
-      else
-      {
-         int value = 0;
-         std::cin >> value;
-         q.enqueue(value);
-      }
-   }
+        if (type == 2)
+        {
+            q.dequeue();
+        }
+        else if (type == 3)
+        {
+            std::cout << q.front() << '\n';
+        }
+        else
+        {
+            int value = 0;
+            std::cin >> value;
+            q.enqueue(value);
+        }
+    }
 
-   return 0;
+    return 0;
 }
