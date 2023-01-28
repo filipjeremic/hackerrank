005aa926f366beae7b41b3471b2f2f903958a5e2
Filip Jeremic <fjeremicwork@gmail.com>
2023-01-28 11:17:52 +0100

    Cleaned up the Python solution
    Problem: Trees: Is This a Binary Search Tree
    Removed the explanation (it's in 'Explanation.txt' now)
    A bit of code reformat

--- Interview Preparation Kit/Trees: Is This a Binary Search Tree?/solution.py
+++ Interview Preparation Kit/Trees: Is This a Binary Search Tree?/solution.py
@@ -1,16 +1,13 @@
-# Recursive solution (naturally, since it’s a tree problem)
-
-# The logic – basically, just bounds checking
-
-# Time complexity: O(n)
-# Space complexity: O(n) - for a balanced BST we would have log(n) here
-
-
-def checkBST_recursive(root, min_value, max_value):
-    if root is None:
+def checkBST_node(node, min_value, max_value):
+    if node is None:
         return True
-    return root.data > min_value and root.data < max_value and checkBST_recursive(root.left, min_value, root.data) and checkBST_recursive(root.right, root.data, max_value)
+    return (
+        node.data > min_value
+        and node.data < max_value
+        and checkBST_node(node.left, min_value, node.data)
+        and checkBST_node(node.right, node.data, max_value)
+    )
 
 
 def checkBST(root):
-    return checkBST_recursive(root, -1, 10001)
+    return checkBST_node(root, 0, 10000)
