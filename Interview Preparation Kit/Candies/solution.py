91254c7f480a89b853746399695cf83ed757895e
Filip Jeremic <fjeremicwork@gmail.com>
2023-02-05 23:03:44 +0100

    Candies Python solution code cleanup

--- Interview Preparation Kit/Candies/solution.py
+++ Interview Preparation Kit/Candies/solution.py
@@ -1,9 +1,8 @@
 #!/bin/python3
 
 # Note - a recursive solution won't work in Python3
-# You will get a stack overflow error
-# And in case you increase the recursion limit you will get a segmentation fault
-# Check out the other languages for a recursive solution
+# A stack overflow error occurs
+# And if the recursion limit is increased, a segmentation fault occurs
 
 
 import os
@@ -11,28 +10,31 @@ import os
 
 def candies(n, arr):
     candy = [1] * n
+
     for i in range(1, n):
         if arr[i] > arr[i - 1]:
             candy[i] = candy[i - 1] + 1
+
     for i in range(n - 2, -1, -1):
-        if arr[i] > arr[i + 1]:
-            candy[i] = max(candy[i], candy[i + 1] + 1)
+        if arr[i] > arr[i + 1] and candy[i] < (candy[i + 1] + 1):
+            candy[i] = candy[i + 1] + 1
+
     return sum(candy)
 
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
-    n = int(input())
+    n = int(input().strip())
 
     arr = []
 
     for _ in range(n):
-        arr_item = int(input())
+        arr_item = int(input().strip())
         arr.append(arr_item)
 
     result = candies(n, arr)
 
-    fptr.write(str(result) + '\n')
+    fptr.write(str(result) + "\n")
 
     fptr.close()
