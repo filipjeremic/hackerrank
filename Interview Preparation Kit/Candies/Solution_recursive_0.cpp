ca42b0acb531616e47b7cad78f7cb118535d7b79
Filip Jeremic <fjeremicwork@gmail.com>
2023-02-05 22:41:04 +0100

    Candies C++ recursive solution code cleanup

--- Interview Preparation Kit/Candies/Solution_recursive_0.cpp
+++ Interview Preparation Kit/Candies/Solution_recursive_0.cpp
@@ -2,20 +2,45 @@
 
 using namespace std;
 
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
+
 pair<long, int> candiesRecursive0(const vector<int> &arr, const size_t index, const int prevCandy)
 {
     const auto n = arr.size();
 
-    if (index == n) return {0, 1};
+    if (index == n)
+    {
+        return {0, 1};
+    }
 
     const int prevBound = ((index > 0) && (arr[index] > arr[index - 1])) ? prevCandy + 1 : 1;
     const auto next = candiesRecursive0(arr, index + 1, prevBound);
     const int nextBound = ((index < n - 1) && (arr[index] > arr[index + 1])) ? next.second + 1 : 1;
     const auto myCandyCount = max(prevBound, nextBound);
+
     return {next.first + myCandyCount, myCandyCount};
 }
 
-// Complete the candies function below.
 long candies(int n, vector<int> arr)
 {
     return candiesRecursive0(arr, 0, 1).first;
@@ -25,17 +50,19 @@ int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
 
-    int n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
 
     vector<int> arr(n);
 
     for (int i = 0; i < n; i++)
     {
-        int arr_item;
-        cin >> arr_item;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        string arr_item_temp;
+        getline(cin, arr_item_temp);
+
+        int arr_item = stoi(ltrim(rtrim(arr_item_temp)));
 
         arr[i] = arr_item;
     }
