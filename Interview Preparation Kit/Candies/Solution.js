35ac76f3bc942c8739ac0677c1e9d181b6966151
Filip Jeremic <fjeremicwork@gmail.com>
2023-02-05 22:56:45 +0100

    Candies JavaScript solution code cleanup

--- Interview Preparation Kit/Candies/Solution.js
+++ Interview Preparation Kit/Candies/Solution.js
@@ -8,11 +8,13 @@ process.stdin.setEncoding('utf-8');
 let inputString = '';
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
+process.stdin.on('data', inputStdin =>
+{
     inputString += inputStdin;
 });
 
-process.stdin.on('end', function () {
+process.stdin.on('end', function ()
+{
     inputString = inputString.replace(/\s*$/, '')
         .split('\n')
         .map(str => str.replace(/\s*$/, ''));
@@ -20,37 +22,44 @@ process.stdin.on('end', function () {
     main();
 });
 
-function readLine() {
+function readLine()
+{
     return inputString[currentLine++];
 }
 
-// Complete the candies function below.
-function candies(n, arr) {
+function candies(n, arr)
+{
     const candy = Array(n).fill(1);
 
-    for (let i = 1; i < n; i++) {
-        if (arr[i] > arr[i - 1]) {
+    for (let i = 1; i < n; i++)
+    {
+        if (arr[i] > arr[i - 1])
+        {
             candy[i] = candy[i - 1] + 1;
         }
     }
 
-    for (let i = n - 2; i >= 0; i--) {
-        if (arr[i] > arr[i + 1]) {
-            candy[i] = Math.max(candy[i], candy[i + 1] + 1);
+    for (let i = n - 2; i >= 0; i--)
+    {
+        if ((arr[i] > arr[i + 1]) && (candy[i] < (candy[i + 1] + 1)))
+        {
+            candy[i] = candy[i + 1] + 1;
         }
     }
 
     return candy.reduce((a, b) => a + b);
 }
 
-function main() {
+function main()
+{
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
     const n = parseInt(readLine(), 10);
 
     let arr = [];
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++)
+    {
         const arrItem = parseInt(readLine(), 10);
         arr.push(arrItem);
     }
