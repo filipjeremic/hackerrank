5aaf91c6e8efac0e84b67f70ff3bafec5b6a34c6
Filip Jeremic <fjeremicwork@gmail.com>
2023-02-07 22:49:26 +0100

    Cleanup

--- Interview Preparation Kit/Max Array Sum/Solution.java
+++ Interview Preparation Kit/Max Array Sum/Solution.java
@@ -1,25 +1,7 @@
-/*
-
-Iterative solution
-
-Time complexity: O(n)
-Space complexity: O(1)
-
-Note - a recursive solution won't work here (stack overflow)
-
-The logic:
-    - every value that you use, the adjacent one you can't
-    - therefore you have to skip everything that is not required
-    - when you skip a value you get the maximum sum so far (this restarts the process of considering values)
-    - negative values are not considered
-    - for positive values you have to check what's larger, max_sum(i - 2) + value or max_sum(i - 1)
-    - if max_sum(i - 2) + value is larger you consider the value (here you have to swap the sums because you added a value)
-    - if max_sum(i - 1) is larger you don't consider value
-
-*/
-
 import java.util.Arrays;
 import java.util.Scanner;
+
+// Note: a recursive solution won't work here (stack overflow)
 
 public class Solution {
 
