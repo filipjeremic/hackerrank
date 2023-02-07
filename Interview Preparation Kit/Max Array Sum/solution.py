3dadcdaa32a9b6b718ae29fae9e5d26984992399
Filip Jeremic <fjeremicwork@gmail.com>
2023-02-07 22:51:50 +0100

    Cleanup

--- Interview Preparation Kit/Max Array Sum/solution.py
+++ Interview Preparation Kit/Max Array Sum/solution.py
@@ -1,20 +1,6 @@
 #!/bin/python3
 
-# Iterative solution
-
-# Time complexity: O(n)
-# Space complexity: O(1)
-
 # Note - a recursive solution won't work here (stack overflow)
-
-# The logic:
-#     - every value that you use, the adjacent one you can't
-#     - therefore you have to skip everything that is not required
-#     - when you skip a value you get the maximum sum so far (this restarts the process of considering values)
-#     - negative values are not considered
-#     - for positive values you have to check what's larger, max_sum(i - 2) + value or max_sum(i - 1)
-#     - if max_sum(i - 2) + value is larger you consider the value (here you have to swap the sums because you added a value)
-#     - if max_sum(i - 1) is larger you don't consider value
 
 
 def maxSubsetSum(arr):
@@ -36,8 +22,7 @@ def maxSubsetSum(arr):
     return max(appendable, notAppendable)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input()
     arr = list(map(int, input().rstrip().split()))
-
     print(maxSubsetSum(arr))
