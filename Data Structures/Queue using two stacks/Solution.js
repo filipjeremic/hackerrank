58d1d6984c56124d60ae4c001f04abf098df41ab
Filip Jeremic <fjeremicwork@gmail.com>
2023-02-07 22:11:59 +0100

    Code cleanup

--- Data Structures/Queue using two stacks/Solution.js
+++ Data Structures/Queue using two stacks/Solution.js
@@ -1,50 +1,65 @@
-class Queue {
-    constructor() {
-        // O(1)
+class Queue
+{
+    // O(1)
+    constructor()
+    {
         this.enqueueStack = [];
         this.dequeueStack = [];
     }
 
-    enqueue(value) {
-        // Amortized O(1)
+    // Amortized O(1)
+    enqueue(value)
+    {
         this.enqueueStack.push(value);
     }
 
-    dequeue() {
-        // Amortized O(1)
+    // Amortized O(1)
+    dequeue()
+    {
         this.check_and_transfer();
         return this.dequeueStack.pop();
     }
 
-    check_and_transfer() {
-        // Amortized O(1)
-        if (this.dequeueStack.length) return;
+    // Amortized O(1)
+    check_and_transfer()
+    {
+        if (this.dequeueStack.length)
+        {
+            return;
+        }
+
         this.transfer();
     }
 
-    transfer() {
-        // Amortized O(n), where n is the number of elements in enqueueStack
-        while (this.enqueueStack.length) {
+    // Amortized O(n), where n is the number of elements in enqueueStack
+    transfer()
+    {
+        while (this.enqueueStack.length)
+        {
             this.dequeueStack.push(this.enqueueStack.pop());
         }
     }
 
-    front() {
-        // Amortized O(1)
+    // Amortized O(1)
+    front()
+    {
         this.check_and_transfer();
         return this.dequeueStack[this.dequeueStack.length - 1];
     }
 }
 
-function processData(input) {
+function processData(input)
+{
     const q = new Queue();
 
     const inputArr = input.trim().split('\n');
 
-    for (let i = 1; i < inputArr.length; i++) {
+    for (let i = 1; i < inputArr.length; i++)
+    {
         const t = inputArr[i].trim().split(' ');
 
-        switch (Number(t[0])) {
+        switch (Number(t[0]))
+        {
             case 2:
                 q.dequeue();
                 break;
@@ -61,10 +76,12 @@ function processData(input) {
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 _input = "";
-process.stdin.on("data", function (input) {
+process.stdin.on("data", function (input)
+{
     _input += input;
 });
 
-process.stdin.on("end", function () {
+process.stdin.on("end", function ()
+{
     processData(_input);
 });
