aba4303143d7fbe78cb313208e7b36cb50270270
Filip Jeremic <fjeremicwork@gmail.com>
2023-02-11 19:46:08 +0100

    Reworked the "Components in a graph" C++ solution

--- Data Structures/Components in a graph/Solution.cpp
+++ Data Structures/Components in a graph/Solution.cpp
@@ -1,132 +1,99 @@
-/*
-
-Time complexity: O(n)
-Space complexity: O(n)
-
-The logic:
-    - you need to keep track of which values are connected (the order is irrelevant)
-    - this is done by having a map which says which value is in which list (lists are unique and shared)
-    - when a new pair is examined there a X cases
-        1) both values are new -> new list and map entries
-        2) both values are not new and they are in the same list already -> do nothing
-        3) both values are not new and they are not in the same list -> update the map entries for
-            the values in one list to point to the other list and add the values from one list to the
-            other list (I chose looping and modifying the smaller list because of performance)
-        4) one value is new -> add it to the list of the other value and create the map entry for it
-
-*/
-
 #include <iostream>
-#include <vector>
-#include <utility>
-#include <unordered_map>
 #include <list>
 #include <memory>
-#include <algorithm>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
-std::vector<std::pair<int, int>> readData()
-{
-    int n = 0;
-    std::cin >> n;
+std::vector<std::pair<int, int>> readData() {
+  std::size_t n = 0;
+  std::cin >> n;
 
-    std::vector<std::pair<int, int>> data;
-    data.reserve(n);
+  std::vector<std::pair<int, int>> data(n);
 
-    for (int i = 0; i < n; i++)
-    {
-        int a = 0;
-        int b = 0;
-        std::cin >> a >> b;
-        data.emplace_back(a, b);
+  for (int i = 0; i < n; i++) {
+    int a = 0;
+    int b = 0;
+    std::cin >> a >> b;
+    data[i] = {a, b};
+  }
+
+  return data;
+}
+
+std::pair<std::size_t, std::size_t> getMinMax(const std::unordered_map<int, std::shared_ptr<std::list<int>>> &m) {
+  std::size_t minSize = m.cbegin()->second->size();
+  std::size_t maxSize = minSize;
+
+  for (const auto &[_, list] : m) {
+    const std::size_t listSize = list->size();
+
+    if (listSize < minSize) {
+      minSize = listSize;
     }
 
-    return data;
+    if (listSize > maxSize) {
+      maxSize = listSize;
+    }
+  }
+
+  return {minSize, maxSize};
 }
 
-auto getSize(const std::pair<int, std::shared_ptr<std::list<int>>>& e)
-{
-    return static_cast<int>(e.second->size());
+std::pair<int, int> componentsInGraph(const std::vector<std::pair<int, int>> data) {
+  std::unordered_map<int, std::shared_ptr<std::list<int>>> m;
+
+  for (const auto [a, b] : data) {
+    const auto iteratorA = m.find(a);
+    const auto iteratorB = m.find(b);
+
+    const bool hasA = (iteratorA != m.cend());
+    const bool hasB = (iteratorB != m.cend());
+
+    if (!hasA && !hasB) {
+      const auto newListSP = std::make_shared<std::list<int>>(std::initializer_list<int>{a, b});
+      m.emplace(a, newListSP);
+      m.emplace(b, newListSP);
+      continue;
+    }
+
+    if (hasA && hasB) {
+      if (iteratorA->second == iteratorB->second) {
+        continue;
+      }
+
+      const auto sizeA = iteratorA->second->size();
+      const auto sizeB = iteratorB->second->size();
+
+      const auto &iteratorSmall = (sizeA < sizeB) ? iteratorA : iteratorB;
+      const auto &iteratorLarge = (sizeA >= sizeB) ? iteratorA : iteratorB;
+
+      const auto smallListSP = iteratorSmall->second;
+
+      auto &largeList = *iteratorLarge->second;
+
+      for (const auto i : *smallListSP) {
+        m[i] = iteratorLarge->second;
+      }
+
+      largeList.splice(largeList.cbegin(), *smallListSP);
+
+      continue;
+    }
+
+    const auto valueToInsert = (hasA ? b : a);
+    const auto insertLocation = (hasA ? iteratorA->second : iteratorB->second);
+    insertLocation->push_front(valueToInsert);
+    m.emplace(valueToInsert, insertLocation);
+  }
+
+  return getMinMax(m);
 }
 
-std::pair<int, int> getMinMaxListSize(const std::unordered_map<int, std::shared_ptr<std::list<int>>>& m)
-{
-    int min = getSize(*m.cbegin());
-    int max = min;
+int main() {
+  const auto data = readData();
+  const auto result = componentsInGraph(data);
+  std::cout << result.first << ' ' << result.second << '\n';
 
-    std::for_each(m.cbegin(), m.cend(), [&min, &max](const auto& e)
-    {
-        const int size = getSize(e);
-        if (size < min) min = size;
-        if (size > max) max = size;
-    });
-
-    return { min, max };
-}
-
-std::pair<int, int> componentsInGraph(const std::vector<std::pair<int, int>> data)
-{
-    std::unordered_map<int, std::shared_ptr<std::list<int>>> m;
-
-    std::for_each(data.cbegin(), data.cend(), [&m](const auto &p)
-    {
-        const int a = p.first;
-        const int b = p.second;
-
-        const auto itEnd = m.cend();
-
-        const auto itA = m.find(a);
-        const auto itB = m.find(b);
-
-        const bool hasA = (itA != itEnd);
-        const bool hasB = (itB != itEnd);
-
-        if (!hasA && !hasB)
-        {
-            const auto listPtr = std::make_shared<std::list<int>>(std::initializer_list<int>{a, b});
-            m.emplace(a, listPtr);
-            m.emplace(b, listPtr);
-
-            return;
-        }
-
-        if (hasA && hasB)
-        {
-            if (itA->second == itB->second) return;
-
-            const auto sizeA = getSize(*itA);
-            const auto sizeB = getSize(*itB);
-
-            const auto& itSmall = (sizeA < sizeB) ? itA : itB;
-            const auto& itLarge = (sizeA >= sizeB) ? itA : itB;
-
-            const auto listSmallSharedPtr = itSmall->second;
-
-            auto& listLarge = *itLarge->second;
-
-            for (const auto i: *listSmallSharedPtr) m[i] = itLarge->second;
-
-            listLarge.splice(listLarge.cbegin(), *listSmallSharedPtr);
-
-            return;
-        }
-
-        const auto valueToInsert = (hasA ? b : a);
-        const auto whereToInsert = (hasA ? itA->second : itB->second);
-
-        whereToInsert->push_front(valueToInsert);
-        m.emplace(valueToInsert, whereToInsert);
-    });
-
-    return getMinMaxListSize(m);
-}
-
-int main()
-{
-    const auto data = readData();
-
-    const auto result = componentsInGraph(data);
-
-    std::cout << result.first << ' ' << result.second << '\n';
-
-    return 0;
+  return 0;
 }
