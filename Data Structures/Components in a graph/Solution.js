da0e0b7b0701018ceaab04634e4385cf4ff9bdad
Filip Jeremic <filipjeremic.dev@gmail.com>
2023-02-13 21:59:29 +0100

    Cleaned up the "Components in a graph" JavaScript solution

--- Data Structures/Components in a graph/Solution.js
+++ Data Structures/Components in a graph/Solution.js
@@ -1,23 +1,5 @@
 'use strict';
 
-/*
-
-Time complexity: O(n)
-Space complexity: O(n)
-
-The logic:
-    - you need to keep track of which values are connected (the order is irrelevant)
-    - this is done by having a map which says which value is in which list (lists are unique and shared)
-    - when a new pair is examined there a X cases
-        1) both values are new -> new list and map entries
-        2) both values are not new and they are in the same list already -> do nothing
-        3) both values are not new and they are not in the same list -> update the map entries for
-            the values in one list to point to the other list and add the values from one list to the
-            other list (I chose looping and modifying the smaller list because of performance)
-        4) one value is new -> add it to the list of the other value and create the map entry for it
-
-*/
-
 const fs = require('fs');
 
 process.stdin.resume();
@@ -27,16 +9,16 @@ let inputString = '';
 let currentLine = 0;
 
 process.stdin.on('data', inputStdin =>
-    {
-        inputString += inputStdin;
-    }
+{
+    inputString += inputStdin;
+}
 );
 
 process.stdin.on('end', _ =>
-    {
-        inputString = inputString.trim().split('\n').map(str => str.trim());
-        main();
-    }
+{
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+    main();
+}
 );
 
 function readLine()
@@ -44,58 +26,64 @@ function readLine()
     return inputString[currentLine++];
 }
 
-function getMinMaxLength(it)
+function getMinMaxLength(connectedComponents)
 {
-    let min = it.next().value.length;
-    let max = min;
+    let minSize = connectedComponents.next().value.length;
+    let maxSize = minSize;
 
-    for (const i of it)
+    for (const component of connectedComponents)
     {
-        const l = i.length;
-        [min, max] = [Math.min(l, min), Math.max(l, max)];
+        const currentSize = component.length;
+        [minSize, maxSize] = [Math.min(currentSize, minSize), Math.max(currentSize, maxSize)];
     }
 
-    return [min, max];
+    return [minSize, maxSize];
 }
 
-function componentsInGraph(gb)
+function componentsInGraph(data)
 {
     const m = new Map();
-    gb.forEach(p =>
+
+    for (const pair of data)
+    {
+        const [a, b] = pair;
+        const [hasA, hasB] = [m.has(a), m.has(b)];
+
+        if (!hasA && !hasB)
         {
-            const [a, b] = p;
-            const [hasA, hasB] = [m.has(a), m.has(b)];
-
-            if (!hasA && !hasB)
-            {
-                const l = [a, b];
-                m.set(a, l);
-                m.set(b, l);
-                return;
-            }
-
-            if (hasA && hasB)
-            {
-                const [la, lb] = [m.get(a), m.get(b)];
-                if (la === lb) return;
-
-                const [sizeA, sizeB] = [la.length, lb.length];
-                const lSmall = (sizeA < sizeB ? la : lb);
-                const lLarge = (sizeA >= sizeB ? la : lb);
-                for (const i of lSmall)
-                {
-                    m.set(i, lLarge);
-                    lLarge.push(i);
-                }
-                return;
-            }
-
-            const valueToInsert = (hasA ? b : a);
-            const whereToInsert = m.get(hasA ? a : b);
-            whereToInsert.push(valueToInsert);
-            m.set(valueToInsert, whereToInsert);
+            const newList = [a, b];
+            m.set(a, newList);
+            m.set(b, newList);
+            continue;
         }
-    );
+
+        if (hasA && hasB)
+        {
+            const [listA, listB] = [m.get(a), m.get(b)];
+
+            if (listA === listB)
+            {
+                continue;
+            }
+
+            const [sizeA, sizeB] = [listA.length, listB.length];
+            const listSmall = (sizeA < sizeB ? listA : listB);
+            const listLarge = (sizeA >= sizeB ? listA : listB);
+
+            for (const i of listSmall)
+            {
+                m.set(i, listLarge);
+                listLarge.push(i);
+            }
+
+            continue;
+        }
+
+        const valueToInsert = (hasA ? b : a);
+        const insertList = m.get(hasA ? a : b);
+        insertList.push(valueToInsert);
+        m.set(valueToInsert, insertList);
+    }
 
     return getMinMaxLength(m.values());
 }
@@ -105,17 +93,15 @@ function main()
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
     const n = parseInt(readLine(), 10);
-
-    let gb = Array(n);
-
-    for (let gbRowItr = 0; gbRowItr < n; gbRowItr++)
+    const data = new Array(n);
+    for (let i = 0; i < n; i++)
     {
-        gb[gbRowItr] = readLine().split(' ').map(gbTemp => parseInt(gbTemp, 10));
+        data[i] = readLine().split(' ').map(x => parseInt(x, 10));
     }
 
-    let result = componentsInGraph(gb);
+    const result = componentsInGraph(data);
 
-    ws.write(result.join(" ") + "\n");
+    ws.write(`${result[0]} ${result[1]}\n`);
 
     ws.end();
 }
