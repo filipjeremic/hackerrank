1ca83f7fac2d23b0d6e3dbaf262e3a5330bbc905
Filip Jeremić <filipjeremic.it@gmail.com>
2025-04-24 18:59:17 +0200

    feat: new 'A or B' C++ solution using the 20 standard

--- algorithms/a-or-b/Solution.cpp
+++ algorithms/a-or-b/Solution.cpp
@@ -1,168 +1,115 @@
-#include <algorithm>
+#include <cctype>
 #include <iostream>
+#include <optional>
 #include <sstream>
+#include <string>
+#include <string_view>
+#include <utility>
 #include <vector>
 
-struct Changes {
-  int m_count = 0;
-  int m_a = 0;
-  int m_b = 0;
+[[nodiscard]]
+static std::vector<bool>
+hexStringToBitVector(const std::string_view hexString) noexcept {
+  std::vector<bool> bitVector;
+  bitVector.reserve(hexString.size() * 4);
 
-  Changes(const int count, const int a, const int b) : m_count(count), m_a(a), m_b(b) {
-  }
-};
-
-static inline int intBit(const int value, const int mask) {
-  return ((value & mask) > 0) ? 1 : 0;
-}
-
-static Changes getMandatoryChanges(const int a, const int b, const int c) {
-  int totalChangeCount = 0;
-  int newA = 0;
-  int newB = 0;
-
-  for (int i = 8; i > 0; i >>= 1) {
-    const int aBit = intBit(a, i);
-    const int bBit = intBit(b, i);
-    const int cBit = intBit(c, i);
-
-    const int changeCount = ((cBit ^ 1) * (aBit + bBit)) + (cBit * ((aBit | bBit) ^ 1));
-
-    totalChangeCount += changeCount;
-    newA = (newA << 1) | (cBit * aBit);
-    newB = (newB << 1) | (cBit * (bBit + changeCount));
-  }
-
-  return {totalChangeCount, newA, newB};
-}
-
-static auto getMandatoryChangesTable() {
-  constexpr size_t N = 16;
-
-  std::vector<std::vector<std::vector<Changes>>> tableA;
-
-  for (size_t a = 0; a < N; a++) {
-    std::vector<std::vector<Changes>> tableB;
-
-    for (size_t b = 0; b < N; b++) {
-      std::vector<Changes> tableC;
-
-      for (size_t c = 0; c < N; c++) {
-        tableC.push_back(getMandatoryChanges(a, b, c));
-      }
-
-      tableB.push_back(tableC);
-    }
-
-    tableA.push_back(tableB);
-  }
-
-  return tableA;
-}
-
-static const auto mandatoryChangesTable = getMandatoryChangesTable();
-
-static int mandatoryChanges(int k, std::vector<int> &a, std::vector<int> &b, const std::vector<int> &c) {
-  const auto size = a.size();
-
-  for (size_t i = 0; i < size; i++) {
-    const auto changes = mandatoryChangesTable[a[i]][b[i]][c[i]];
-
-    k -= changes.m_count;
-    if (k < 0)
-      break;
-
-    a[i] = changes.m_a;
-    b[i] = changes.m_b;
-  }
-
-  return k;
-}
-
-static Changes optimize(int k, const int a, const int b, const int c) {
-  int changeCountTotal = 0;
-  int newA = 0;
-  int newB = 0;
-
-  for (int i = 8; i > 0; i >>= 1) {
-    const int aBit = intBit(a, i);
-    const int bBit = intBit(b, i);
-    const int cBit = intBit(c, i);
-    const int changeCount = aBit + (bBit ^ 1);
-
-    const bool hasChanges = ((cBit == 1) && (changeCount <= k));
-
-    newA = (newA << 1) | (hasChanges ? 0 : aBit);
-    newB = (newB << 1) | (hasChanges ? 1 : bBit);
-
-    if (hasChanges) {
-      k -= changeCount;
-      changeCountTotal += changeCount;
+  for (const char c : hexString) {
+    const int hexValue = c - (std::isdigit(c) ? '0' : 'A' - 10);
+    for (int i = 3; i >= 0; i--) {
+      bitVector.push_back(((hexValue >> i) & 1) != 0);
     }
   }
 
-  return {changeCountTotal, newA, newB};
+  return bitVector;
 }
 
-static void optimizationChanges(int k, std::vector<int> &a, std::vector<int> &b, const std::vector<int> &c) {
-  for (size_t i = 0; i < a.size(); i++) {
-    if (k == 0)
-      break;
-
-    auto changes = optimize(k, a[i], b[i], c[i]);
-
-    k -= changes.m_count;
-
-    a[i] = changes.m_a;
-    b[i] = changes.m_b;
-  }
-}
-
-static void aOrB(int k, const std::string &a, const std::string &b, const std::string &c) {
-  const size_t size = a.size();
-
-  auto stringToIntVector = [](const std::string &s, std::vector<int> &v) {
-    std::transform(std::cbegin(s), std::cend(s), std::begin(v),
-                   [](const char c) -> int { return ((c >= 'A') && (c <= 'Z')) ? (c - 'A' + 10) : (c - '0'); });
-  };
-
-  std::vector<int> vectorA(size, 0);
-  std::vector<int> vectorB(size, 0);
-  std::vector<int> vectorC(size, 0);
-
-  stringToIntVector(a, vectorA);
-  stringToIntVector(b, vectorB);
-  stringToIntVector(c, vectorC);
-
-  k = mandatoryChanges(k, vectorA, vectorB, vectorC);
-
-  if (k < 0) {
-    std::cout << "-1\n";
-    return;
-  }
-
-  optimizationChanges(k, vectorA, vectorB, vectorC);
-
-  auto print = [](const std::vector<int> &v) {
-    bool skipZero = true;
-
-    std::stringstream ss;
-
-    for (const int i : v) {
-      if ((i == 0) && skipZero)
-        continue;
+[[nodiscard]]
+static std::string
+bitVectorToHexString(const std::vector<bool> &bitVector) noexcept {
+  std::stringstream ss;
+  bool skipZero = true;
+  for (std::size_t i = 0; i < bitVector.size(); i += 4) {
+    int hexValue = 0;
+    for (std::size_t j = 0; j < 4; j++) {
+      hexValue = (hexValue << 1) | bitVector[i + j];
+    }
+    if (!skipZero || hexValue != 0) {
       skipZero = false;
-      ss << static_cast<char>((i < 10) ? ('0' + i) : ('A' - 10 + i));
+      constexpr std::string_view hexDigits = "0123456789ABCDEF";
+      ss << hexDigits[hexValue];
+    }
+  }
+
+  std::string hexString = ss.str();
+  return hexString.empty() ? "0" : hexString;
+}
+
+[[nodiscard]]
+static bool getInitialResult(int &k, std::vector<bool> &aBitVector,
+                             std::vector<bool> &bBitVector,
+                             const std::vector<bool> &cBitVector) noexcept {
+  for (std::size_t i = 0; i < aBitVector.size(); i++) {
+    const bool a = aBitVector[i];
+    const bool b = bBitVector[i];
+    const bool c = cBitVector[i];
+
+    aBitVector[i] = a && c;
+    bBitVector[i] = c && (!a || (a && b));
+
+    const int da = a == aBitVector[i] ? 0 : 1;
+    const int db = b == bBitVector[i] ? 0 : 1;
+    k -= da + db;
+
+    if (k < 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static void minimizeA(int &k, std::vector<bool> &aBitVector,
+                      std::vector<bool> &bBitVector) noexcept {
+  for (std::size_t i = 0; i < aBitVector.size(); i++) {
+    if (k == 0) {
+      return;
     }
 
-    if (skipZero)
-      ss << '0';
+    if (!aBitVector[i]) {
+      continue;
+    }
 
-    std::cout << ss.str() << '\n';
-  };
+    if (bBitVector[i]) {
+      k--;
+    } else if (k >= 2) {
+      k -= 2;
+    } else {
+      continue;
+    }
 
-  print(vectorA);
-  print(vectorB);
+    aBitVector[i] = false;
+    bBitVector[i] = true;
+  }
+}
+
+[[nodiscard]]
+static std::optional<std::pair<std::string, std::string>>
+a_or_b(int k, const std::string_view aHexString,
+       const std::string_view bHexString,
+       const std::string_view cHexString) noexcept {
+
+  std::vector<bool> aBitVector = hexStringToBitVector(aHexString);
+  std::vector<bool> bBitVector = hexStringToBitVector(bHexString);
+  const std::vector<bool> cBitVector = hexStringToBitVector(cHexString);
+
+  if (!getInitialResult(k, aBitVector, bBitVector, cBitVector)) {
+    return std::nullopt;
+  }
+
+  minimizeA(k, aBitVector, bBitVector);
+
+  return std::make_pair(bitVectorToHexString(aBitVector),
+                        bitVectorToHexString(bBitVector));
 }
 
 int main() {
@@ -175,7 +122,12 @@ int main() {
 
     std::cin >> k >> a >> b >> c;
 
-    aOrB(k, a, b, c);
+    if (const auto result = a_or_b(k, a, b, c)) {
+      std::cout << result.value().first << '\n'
+                << result.value().second << std::endl;
+    } else {
+      std::cout << -1 << std::endl;
+    }
   }
 
   return 0;
